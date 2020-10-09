07834e6e623c3c51da2cff0a74b48377a330e4e9
jakechv <jakechvatal@gmail.com>
2020-10-09 17:13:35 +0200

    Add notes on greater problem complexity

--- promise/independentPromises.js
+++ promise/independentPromises.js
@@ -1,3 +1,58 @@
+/* Collects all of the literals in an argument list.
+ * These arguments should be in scope. */
+const flattenArguments = (arguments) => 
+    arguments.reduce((acc, argument) => {
+        switch(argument.type) {
+            case 'Identifier':
+                return acc.append(argument.name);
+            case 'Literal':
+                return acc;
+            case 'ArrayExpression':
+                return acc.append(flattenArguments(argument.elements));
+            default:
+                return acc;
+        }
+    }, []);
+
+/* Determine whether this call expression is independent
+ * (it has no arguments in the flattened argument list). */
+const isIndependentCallExpression = (expression, acc) =>
+        flattenArguments(expression.arguments).reduce((prev, arg) => arg in acc && prev, true);
+
+/* Determines whether this node is an 'await'. */
+const isAwait = node => null;
+
+/* Determine whether this node is an assignment to a variable from an 'await'. */
+const isAwaitWithAssignment = node => null;
+
+const getVariableAssignedTo = node => {
+    if(isAwaitWithAssignment(node)) {
+    }
+}
+
+// linearly, we:
+// - check scope of variables on right side of expression, then
+// - collect variables on left side of expression
+// - if we *do not* see any of the variables we've selected,
+//   and we see more than one independent call,
+//   we provide a warning stating that the independent calls can be coupled
+//
+// it's not that simple!
+//
+// consider
+// x = await fun(a);
+// y = fun(x)
+// z = await fun(y)
+//
+// This logic would say that these are independent,
+// but they definitely are not
+// we have to construct a dependency graph of function expressions and their arguments,
+// then determine if an anscestor of any current variable was an 'await' expression
+
+// we also don't have any information about incoming arguments;
+// what if an incoming argument is an awaited expression that hasn't been evaluated?
+// I suppose we can ignore it, but that isn't very good form; it would be better to be more robust.
+
 const independentPromises = {
     meta: {
         fixable: false
@@ -12,35 +67,6 @@ const independentPromises = {
             }:
         };
     },
-
-    // in all chunks of code:
-    // - for each expressionStatement:
-    // - if it's an awaitExpression:
-    // -
-
-
-    /* Get all of the literals in an argument list. */
-    const flattenArguments = (arguments) => 
-        arguments.reduce((acc, argument) => {
-            switch(argument.type) {
-                case 'Identifier':
-                    return acc.append(argument.name);
-                case 'Literal':
-                    return acc;
-                case 'ArrayExpression':
-                    return acc.append(flattenArguments(argument.elements));
-                default:
-                    return acc;
-            }
-        }, []);
-
-    // ASSUME:
-    // - CallExpression
-    // - in 'await'
-
-    /* Determine whether this call expression is independent. */
-    const isIndependentCallExpression = (expression, acc) =>
-          flattenArguments(expression.arguments).reduce((prev, arg) => arg in acc && prev, true);
 }
 
 module.exports = {
